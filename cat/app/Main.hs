62cc10bf477a527737716fbd01ec3d3b9576e882
Ayumu3746221 <a.kkt0307@gmail.com>
2026-02-17 16:59:22 +0900

    cat commandを実装しました

---
+++ cat/app/Main.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  contents <- case args of
+    [path] -> TIO.readFile path
+    _ -> TIO.getContents
+  TIO.putStr contents
